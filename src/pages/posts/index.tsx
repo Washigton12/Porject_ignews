f213ffa552e245e05c4cad8c302a4245f527b567
Washigton Carvalho <tomcsilva12@gmail.com>
2022-01-06 23:21:55 -0300

    CMS

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,43 @@
+import Head from 'next/head'
+import styles from './styles.module.scss'
+
+export default function Posts() {
+  return (
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+
+      <main>
+        <div>
+          <a>
+            <time>06 de Janeiro de 2022</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>
+              In this guide, you will learn how to create a Monorepo to manage
+              multiple packages with a shared build, test, and release process.
+            </p>
+          </a>
+
+          <a>
+            <time>06 de Janeiro de 2022</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>
+              In this guide, you will learn how to create a Monorepo to manage
+              multiple packages with a shared build, test, and release process.
+            </p>
+          </a>
+
+          <a>
+            <time>06 de Janeiro de 2022</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>
+              In this guide, you will learn how to create a Monorepo to manage
+              multiple packages with a shared build, test, and release process.
+            </p>
+          </a>
+        </div>
+      </main>
+    </>
+  )
+}
