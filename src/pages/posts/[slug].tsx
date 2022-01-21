874f81452a2e216178b5afed02e9ca7a0fa721c8
Washigton Carvalho <tomcsilva12@gmail.com>
2022-01-20 22:56:14 -0300

    Pagina: Preview do post

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -41,7 +41,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
   const session = await getSession({ req });
   const { slug } = params;
   
-  if (!session || !session.activeSubscription) {
+  if (!session || !session?.activeSubscription) {
     return {
       redirect: {
         destination: '/',
